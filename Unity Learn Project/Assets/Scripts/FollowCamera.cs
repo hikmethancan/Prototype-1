6ce24ddb33e4b528ce2b8c655bb4dd8779e2d101
hikmethancan <hikmethann@gmail.com>
2021-07-22 21:38:01 +0300

    commit

---
+++ Unity Learn Project/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public GameObject vehicle;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0, 7, -12);
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = vehicle.transform.position + cameraOffset;
+    }
+}
---
+++ Unity Learn Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
